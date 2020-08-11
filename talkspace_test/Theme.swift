38390c45526d2a9b2923a9461a7385158e4a738a
David Arvelo <david@davidarvelo.com>
2020-08-10 23:55:19 -0400

    Add Theme

---
+++ talkspace_test/Theme.swift
@@ -0,0 +1,22 @@
+//
+//  Theme.swift
+//  talkspace_test
+//
+//  Created by David Arvelo on 8/10/20.
+//  Copyright © 2020 David Arvelo. All rights reserved.
+//
+
+import UIKit
+
+// TODO: This should be passed down through every view controller,
+//       rather than referenced explicitly.
+struct Theme {
+    struct ToolbarView {
+        let widthFactor: CGFloat = 0.9
+        let height: CGFloat = 70
+        let bottomOffset: CGFloat = -20
+        let colorViewSize = CGSize(width: 50, height: 50)
+    }
+
+    static let toolbarView = ToolbarView()
+}
